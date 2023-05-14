8cd5e542ab503ef0b67ca864e279b1f0275ecfca
NuclearLion <dando.ds11@gmail.com>
2023-05-14 19:12:57 +0300

    task 4 done

--- src/task-4/checkers.asm
+++ src/task-4/checkers.asm
@@ -17,6 +17,151 @@ checkers:
     ;; DO NOT MODIFY
     ;; FREESTYLE STARTS HERE
 
+	mov esi, ecx
+	xor ecx, ecx
+
+;address = base_address + (row_index * number_of_columns + column_index)
+
+upper_left:
+	push eax
+	push ebx
+	push esi
+
+	inc eax
+	dec ebx
+
+	cmp eax, 0
+	jl no_place_ul
+
+	cmp eax, 7
+	jg no_place_ul
+
+	cmp ebx, 0
+	jl no_place_ul
+
+	cmp ebx, 7
+	jg no_place_ul
+
+	mov dx, 8
+	mul dx
+
+	add ax, bx
+
+	add esi, eax
+
+	mov dword [esi], dword 1
+
+	
+no_place_ul:
+	pop esi
+	pop ebx
+	pop eax
+
+
+upper_right:
+	push eax
+	push ebx
+	push esi
+
+	inc eax
+	inc ebx
+
+	cmp eax, 0
+	jl no_place_ur
+
+	cmp eax, 7
+	jg no_place_ur
+
+	cmp ebx, 0
+	jl no_place_ur
+
+	cmp ebx, 7
+	jg no_place_ur
+
+	mov dx, 8
+	mul dx
+
+	add ax, bx
+
+	add esi, eax
+
+	mov dword [esi], dword 1
+
+no_place_ur:
+	
+	pop esi
+	pop ebx
+	pop eax
+
+lower_left:
+	push eax
+	push ebx
+	push esi
+
+	dec eax
+	dec ebx
+
+	cmp eax, 0
+	jl no_place_ll
+
+	cmp eax, 7
+	jg no_place_ll
+
+	cmp ebx, 0
+	jl no_place_ll
+
+	cmp ebx, 7
+	jg no_place_ll
+
+	mov dx, 8
+	mul dx
+
+	add ax, bx
+
+	add esi, eax
+
+	mov dword [esi], dword 1
+
+no_place_ll:
+	pop esi
+	pop ebx
+	pop eax
+
+
+lower_right:
+	push eax
+	push ebx
+	push esi
+
+	dec eax
+	inc ebx
+
+	cmp eax, 0
+	jl no_place_lr
+
+	cmp eax, 7
+	jg no_place_lr
+
+	cmp ebx, 0
+	jl no_place_lr
+
+	cmp ebx, 7
+	jg no_place_lr
+
+	mov dx, 8
+	mul dx
+
+	add ax, bx
+
+	add esi, eax
+
+	mov dword [esi], dword 1
+
+
+no_place_lr:
+	pop esi
+	pop ebx
+	pop eax
 
     ;; FREESTYLE ENDS HERE
     ;; DO NOT MODIFY
